e75b8fe9ad48fc1e47f0d250008d4c5b613eecc7
jakub.salek <jsalek02@gmail.com>
2023-12-03 23:04:41 +0100

    Dodanie Main Menu, Settings i Credits

---
+++ StarSettlers/Scenes/Main Menu/MainMenu.gd
@@ -0,0 +1,27 @@
+extends CanvasLayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_new_game_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+
+func _on_continue_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
+
+
+func _on_settings_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Main Menu/Settings.tscn")
+
+
+func _on_exit_button_pressed():
+	get_tree().quit()
---
+++ StarSettlers/Scenes/Main Menu/Settings.gd
@@ -0,0 +1,11 @@
+extends CanvasLayer
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Main Menu/MainMenu.tscn")
+
+
+
+func _on_credits_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Main Menu/Credits.tscn")
+
+
