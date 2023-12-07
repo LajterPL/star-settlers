a39c17000d12ef9770c6776001c07c162daa80a1
Kacper Prządka <kacper.przadka@o2.pl>
2023-12-07 13:07:13 +0100

    brakujący plik

---
+++ StarSettlers/Scenes/Map/Path.gd
@@ -0,0 +1,11 @@
+extends Node2D
+@onready var player = $"../Player"
+
+func _process(_delta):
+	pass
+	#queue_redraw()
+
+func _draw():
+	if player.current_point_path.is_empty():
+		return
+	draw_polyline(player.current_point_path, Color.RED,2)
