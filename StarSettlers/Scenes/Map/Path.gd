e9bbf3db8ca7fe1e887a5f2b3f0e732af0212863
Kacper Prządka <kacper.przadka@o2.pl>
2023-12-07 13:33:18 +0100

    dodanie parametrów wyświetlania ścieżki dla A*

--- StarSettlers/Scenes/Map/Path.gd
+++ StarSettlers/Scenes/Map/Path.gd
@@ -2,10 +2,9 @@ extends Node2D
 @onready var player = $"../Player"
 
 func _process(_delta):
-	pass
-	#queue_redraw()
+	queue_redraw()
 
 func _draw():
-	if player.current_point_path.is_empty():
+	if player.current_point_path.is_empty() or !get_meta("show"):
 		return
-	draw_polyline(player.current_point_path, Color.RED,2)
+	draw_polyline(player.current_point_path, get_meta("color"),get_meta("width"))
