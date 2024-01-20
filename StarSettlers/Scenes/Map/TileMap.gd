f603aeefcbeb22274180dcf2e45f023bc729c28d
Kacper Prządka <kacper.przadka@o2.pl>
2024-01-20 15:06:59 +0100

    generowanie biomów z losowym odcieniem podłoża (serio przepraszam za te kolory)

--- StarSettlers/Scenes/Map/TileMap.gd
+++ StarSettlers/Scenes/Map/TileMap.gd
@@ -1,73 +1,95 @@
 extends TileMap
 @onready var player = $"../Player"
+# id warstwy podłoża
 var ground_layer = 0
 var map_size = Vector2i(150,150)
-var biome_colour = []
-var radius = map_size.x/4
-# biom kolor jest tymczasowe by wizualizować inne biomy
+# dystans od środka mapy w jakim rozpoczynam generacje innych biomów
+var radius = map_size.x/4 
 
 func _ready():
-	biome_colour.append(Vector2i(0,0))
-	biome_colour.append(Vector2i(0,1))
-	biome_colour.append(Vector2i(0,2))
-	biome_colour.append(Vector2i(1,0))
-	biome_colour.append(Vector2i(1,1))
-	biome_colour.append(Vector2i(1,2))
 	generate_bioms_layout()
 
-	
+## główna funkcja generowania mapy
+## iteruje po cistach krawędzi i wypełnia tak długo jak ma miejsce
+## biom początkowy dodatkowo ograniczam dystansem
 func generate_bioms_layout():
 	var number_of_bioms = 5
 	var biom_start_positions = biom_start_points(number_of_bioms)
 	var fill_counter = map_size.x * map_size.y
 	var biom_pointer = 0
 	var edges = []
-	for i in range(0,number_of_bioms+1):
+
+	for i in range(0, number_of_bioms + 1):
 		edges.append([])
 		edges[i].append(biom_start_positions[i])
+
 	while fill_counter > 0:
-		if edges[biom_pointer].size()>0:
-			var tile_index = randi()%edges[biom_pointer].size()
-			if not(biom_pointer == 0 and vec_distance(edges[biom_pointer][tile_index],Vector2i(int(map_size.x/2), int(map_size.y/2)))>radius): 
-				edges[biom_pointer] = set_empty_neighbours(tile_index, edges[biom_pointer],biome_colour[biom_pointer])		
-		fill_counter-=1
-		biom_pointer = (biom_pointer+1)%(number_of_bioms+1)
+		if edges[biom_pointer].size() > 0:
+			var tile_index = randi() % edges[biom_pointer].size()
+			
+			if not (biom_pointer == 0 and
+					vec_distance(edges[biom_pointer][tile_index],
+								 Vector2i(int(map_size.x / 2), int(map_size.y / 2))) > radius
+			):
+				edges[biom_pointer] = set_empty_neighbours(
+					tile_index, edges[biom_pointer],
+					Vector2i(randi() % 4, biom_pointer)
+				)
+
+		fill_counter -= 1
+		biom_pointer = (biom_pointer + 1) % (number_of_bioms + 1)
+
 
 func vec_distance(a: Vector2i, b: Vector2i):
-	return sqrt(pow((a.x-b.x),2)+pow((a.y-b.y),2))
-	
-	
-func set_empty_neighbours(tile_index: int, biom_edge_set,biom_colour):
+	return sqrt(pow((a.x - b.x), 2) + pow((a.y - b.y), 2))
+
+## ustawi pustych sąsiadów komórki jako kafelek o podanym kolorze
+## i dopisuje do listy krawędzi biomu
+func set_empty_neighbours(tile_index: int, biom_edge_set, biom_colour):
 	var tile = biom_edge_set[tile_index]
 	biom_edge_set.erase(tile)
+
 	for neighbor in get_surrounding_cells(tile):
-		if get_cell_tile_data(ground_layer,neighbor) == null and neighbor.x>0 and neighbor.y>0 and neighbor.x<map_size.x and neighbor.y<map_size.y:
-			set_cell(ground_layer,neighbor,2,biom_colour)
+		if (
+			get_cell_tile_data(ground_layer, neighbor) == null
+			and neighbor.x > 0
+			and neighbor.y > 0
+			and neighbor.x < map_size.x
+			and neighbor.y < map_size.y
+		):
+			set_cell(ground_layer, neighbor, 3, biom_colour)
 			biom_edge_set.append(neighbor)
-			
+
 	for elem in biom_edge_set:
 		var covered = true
 		for neighbor in get_surrounding_cells(elem):
-			if get_cell_tile_data(ground_layer,neighbor) == null:
+			if get_cell_tile_data(ground_layer, neighbor) == null:
 				covered = false
 				break
+
 		if covered:
 			biom_edge_set.erase(elem)
+
 	return biom_edge_set
-	
+
+## ustawia koordynasy punktów rozpoczącia generacji biomów
 func biom_start_points(number_of_bioms: int):
 	var biom_start_positions = []
-	var center = Vector2i(int(map_size.x/2), int(map_size.y/2))
+	var center = Vector2i(int(map_size.x / 2), int(map_size.y / 2))
 	biom_start_positions.append(center)
+
 	var base_angle = 10
 	var angle = randi()
+
 	for point in range(0, number_of_bioms):
-		angle+= base_angle
+		angle += base_angle
 		var x = int(center.x + radius * cos(angle))
 		var y = int(center.y + radius * sin(angle))
-		biom_start_positions.append(Vector2i(x,y))
+		biom_start_positions.append(Vector2i(x, y))
+
 	return biom_start_positions
-	
+
+
 	
 func a_star_path(start: Vector2i, target: Vector2i):
 	var open_set = []
